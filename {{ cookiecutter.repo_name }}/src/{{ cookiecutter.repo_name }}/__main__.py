af79f6038c27fe18b7fee40aa39557e5c2dff3ab
mj0ln1r42 <baileybp4@gmail.com>
2024-03-12 20:09:42 -0400

    add __main__.py

---
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.repo_name }}/__main__.py
@@ -0,0 +1,18 @@
+import argparse
+from loguru import logger
+
+
+def main(args):
+    logger.debug(args)
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(
+		description="{{ cookiecutter.project_description }}"
+	)
+
+	# Add args here
+    # parser.add_argument('--integers', type=int, help='an integer for the accumulator')
+
+	args = parser.parse_args()
+	main(args)
